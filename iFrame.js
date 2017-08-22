97b4c348dbc04dbb3959e9bf9be851d27ddc6e78
Dave Mitchell <davidbmitchell77@outlook.com>
2017-08-22 13:06:53 -0700

    Embedded iframe example code

--- iFrame.js
+++ iFrame.js
@@ -1,18 +1,31 @@
-// iFrame/parent window  2-way communication sample code:
+//  Visualforce markup for iframe useage:
+  <apex:page controller="iframeController" sidebar="false">
+    <apex:inputText id="message"/>
+    <iframe src="{!url.url__c}" scrolling="true" id="iframe" width="100%" height="100%"/>
+  </apex:page>
 
-//parent window sends message to remote site through the iFrame:
+// Visualforce markup for iframe w/ auto-resizing:
+  <apex:page sidebar="false">
+    <apex:inputText id="message"/><p/>
+    <apex:iframe src="https://trailhead.salesforce.com/" id="iframe" />
+    <script>document.getElementById('iframe').height = window.innerHeight - 220;</script>
+  </apex:page>
+
+/* ----------------------------------------------------------- */
+/* iFrame/parent window  2-way communication sample code:      */
+/* ----------------------------------------------------------- */
+// parent window sends message to remote site through the iFrame:
 document.getElementById("iframe").contentWindow.postMessage(
-        document.getElementById("message").value,
-        "http://anotherdomain.com" // restrict message handling to this remote URL
+    document.getElementById("message").value,  // text from parent window text box
+    "http://anotherdomain.com"   // restrict message handling to this remote domain
 );
 
-// remote site handles the message with:
-window.onmessage = function(e){
-  // if not the authorized domain, do not process the message
+// remote site handles the message:
+window.onmessage = function(e) {
+  // if message not sent fom authorized domain, do not process:
   if ( e.origin !== "http://html5demos.com" ) {
     return;
   }
-
-  // display the message sent from parent window:
+  // display message sent from parent window in the child iframe:
   document.getElementById("test").innerHTML = e.origin + " said: " + e.data;
 };
